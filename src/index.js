4c3bbaeace61b79fc57411d42bb61e425df8c638
shresth2405 <maheshwarishresth@gmail.com>
2025-02-05 01:18:59 +0530

    MongoDB added

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// require("dotenv").config({path:'./env '});
+import dotenv from "dotenv";
+import mongoose from "mongoose"
+import {DB_NAME} from "./constants.js"
+import express from "express"
+import connectDB from "./db/index.js";
+
+
+const app=express();
+dotenv.config({
+    path:"./env"
+})
+
+connectDB()
+
+app.listen(process.env.PORT,()=>{
+         console.log(`Port is listening on port ${process.env.PORT}`)
+})
+
+
+
+
+
+// ;(async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("Error:",error)
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Port is listening on port ${process.env.PORT}`)
+//         })
+//     }catch(error){
+//         console.log("Error:", error);
+//     }
+// })()
