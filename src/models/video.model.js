85f51affe0cc935c33b8f21d02be0de415c51626
shresth2405 <maheshwarishresth@gmail.com>
2025-02-09 17:11:42 +0530

    Added models

---
+++ src/models/video.model.js
@@ -0,0 +1,52 @@
+import mongoose,{Schema} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const videoSchema =new Schema({
+    videoFile:{
+        type:String,
+        required:true,  
+        index:true
+    },
+    title:{
+        type:String,
+        required:true,  
+        index:true
+    },
+    description:{
+        type:String,
+        required:true,
+        unique:true,  
+        index:true
+    },
+    duration:{
+        type:Number,
+        required:true,
+    },
+    views:{
+        type:Number,
+        required:true,
+        default:0 
+    },
+    isPublished:{
+        type:Boolean,
+        default:true,
+        required:true,
+    },
+    thumbnail:{
+        type:String,
+        required:true, 
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    }
+},
+{
+    timestamps:true
+}
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+const Video=new model("Video", videoSchema)
